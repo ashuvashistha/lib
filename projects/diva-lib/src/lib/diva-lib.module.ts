86506322908e441766af5646fa69cfe137cd5614
Ashish Sharma <ashish.sharma@metacube.com>
2020-03-26 16:51:35 +0530

    initial commit

--- projects/diva-lib/src/lib/diva-lib.module.ts
+++ projects/diva-lib/src/lib/diva-lib.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { DivaLibComponent } from './diva-lib.component';
 import { HeaderComponent } from './header/header.component';
+import {
+  MatSidenavModule,
+  MatTableModule,
+  MatSelectModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [DivaLibComponent, HeaderComponent],
